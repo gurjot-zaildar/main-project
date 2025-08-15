b35f48108df93f971a51edebc75a752077594957
gurjot-zaildar <signingames115@gmail.com>
2025-08-15 22:54:32 +0530

    Add task input form and styling for task list functionality

--- project_1/script.js
+++ project_1/script.js
@@ -1,4 +1,5 @@
-let allelems = document.querySelectorAll(".elem");
+function openfeatures(){
+    let allelems = document.querySelectorAll(".elem");
 let fullelemspages = document.querySelectorAll(".fullelem");
 let fullelemsbackbtn = document.querySelectorAll(".fullelem .back");
 
@@ -12,4 +13,48 @@ fullelemsbackbtn.forEach(function(back){
 back.addEventListener("click",function(){
     fullelemspages[back.id].style.display="none"
 })
+})
+}
+openfeatures()
+
+let form= document.querySelector(".addtask form");
+let taskinput=document.querySelector(".addtask form input")
+let taskdetailsinput=document.querySelector(".addtask form textarea")
+let taskcheckbox=document.querySelector(".addtask form #check")
+
+let currenttask = [{
+        task : "gym jyo",
+        details: "mai bhi ja raha hu",
+        check: true
+},
+{
+     task : " jyo",
+        details: "mai bhi",
+        check: false
+}]
+
+
+function rendertask(){
+    let alltask = document.querySelector(".alltask");
+    
+    let sum = "";
+    
+    currenttask.forEach(function(elem){
+        sum+= `<div class="task">
+        <h1>${elem.task} <span class="${elem.check}">imp</span></h1>
+        <button>Mark as Completed</button>
+        </div>`
+    })
+    
+    alltask.innerHTML=sum;
+}
+rendertask()
+
+
+form.addEventListener("submit",function(e){
+    e.preventDefault()
+    // console.log(taskinput.value)
+    // console.log(taskdetailsinput.value)
+    // console.log(taskcheckbox.checked)
+    currenttask.push()
 })
