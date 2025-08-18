75b87f50f1e431e41a376179be01cefccc596371
gurjot-zaildar <signingames115@gmail.com>
2025-08-18 15:08:04 +0530

    Add motivation and Pomodoro timer sections with styling and functionality

--- project_1/script.js
+++ project_1/script.js
@@ -108,4 +108,89 @@ localStorage.setItem("dayplandata",JSON.stringify(dayplandata))
 
 dailypln()
 
+// function moti(){
+    
+// let motivationcontent= document.querySelector(".motivation-2 h1")
+// let motivationauthor = document.querySelector(".motivation-3 h1")
 
+// async function fetchquotes() {
+//     let response= await fetch('https://api.zenquotes.io/random')
+//     let data = await response.json()
+
+//     motivationcontent.innerHTML= data.content
+//     motivationauthor.innerHTML= data.author
+// }
+// fetchquotes()
+// }
+
+// moti()
+
+
+
+function potimer(){
+
+let session= document.querySelector(".session")
+let time= document.querySelector(".timer h1")
+let startbtn=document.querySelector(".start")
+let pausebtn=document.querySelector(".pause")
+let resetbtn=document.querySelector(".reset")
+
+let isworksession = true
+
+let totalsecound = 25*60
+let timeinterval =null
+
+function updatetimer(){
+    let minutes = Math.floor(totalsecound/60)
+    let seconds = totalsecound %60
+
+    time.innerHTML =`${String(minutes).padStart("2","0")}:${String(seconds).padStart("2","0")}`
+}
+
+function settimer(){
+    clearInterval(timeinterval)
+    if(isworksession){
+        timeinterval = setInterval(function(){
+            if(totalsecound>0){
+                totalsecound--
+                updatetimer()
+            }else{
+                isworksession =false;
+                clearInterval(timeinterval)
+                time.innerHTML = "05:00"
+                session.innerHTML="take a break"
+                session.style.backgroundColor = "blue"
+                totalsecound = 5*60
+            }
+        },1000)
+    }else{
+        timeinterval=setInterval(function(){
+            if(totalsecound>0){
+                totalsecound--
+                updatetimer()
+            }else{
+                isworksession= true
+                clearInterval(timeinterval)
+                time.innerHTML="25:00"
+                session.style.backgroundColor="green"
+                session.innerHTML="worksession"
+                totalsecound= 25*60
+            }
+        },1000)
+    }
+}
+
+function pause(){
+    clearInterval(timeinterval)
+}
+function reset(){
+    totalsecound=25*60
+    clearInterval(timeinterval)
+    updatetimer()
+}
+    startbtn.addEventListener('click', settimer)
+    pausebtn.addEventListener('click', pause)
+    resetbtn.addEventListener('click', reset)
+}
+
+potimer()
